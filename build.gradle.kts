2a3859b3b6e574242755a132addc230c017cae89
Juraji <github@juraji.nl>
2020-11-30 21:41:41 +0100

    Added first draft of Github Actions workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 group = "nl.juraji"
 version = "1.0.0"
 java.sourceCompatibility = JavaVersion.VERSION_14
-java.targetCompatibility = JavaVersion.VERSION_11
+java.targetCompatibility = JavaVersion.VERSION_14
 
 repositories {
     mavenCentral()
@@ -16,17 +16,12 @@ repositories {
 dependencies {
     val projectReactor = "3.4.0"
     val junit = "5.7.0"
-    val mockk = "1.10.2"
 
-//    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlin")
-//    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin")
     implementation("io.projectreactor:reactor-core:$projectReactor")
 
     testImplementation("org.junit.jupiter:junit-jupiter:$junit")
     testImplementation("io.projectreactor:reactor-test:$projectReactor")
-    testImplementation("io.mockk:mockk:$mockk")
 }
-
 tasks.withType<Test> {
     useJUnitPlatform()
 }
@@ -36,4 +31,8 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = java.targetCompatibility.majorVersion
     }
+}
+
+tasks.register("version") {
+    println(version)
 }
