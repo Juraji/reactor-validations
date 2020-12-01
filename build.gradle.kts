fb49b4e50c4233f29484497c9ebb281f20af7aa5
Juraji <github@juraji.nl>
2020-12-01 01:33:22 +0100

    Added publishing to github packages to build.gradle.kts and workflow.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.20"
-    maven
+    id("maven")
+    id("maven-publish")
 }
 
 group = "nl.juraji"
@@ -23,6 +24,47 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:$junit")
     testImplementation("io.projectreactor:reactor-test:$projectReactor")
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "GithubPackages"
+            url = uri("https://maven.pkg.github.com/Juraji/reactor-validations")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                name.set("Reactor Validations")
+                description.set("A Kotlin based validations api with Project Reactor extensions")
+                url.set("https://github.com/Juraji/reactor-validations")
+                licenses {
+                    license {
+                        name.set("GNU General Public License v3.0")
+                        url.set("https://github.com/Juraji/reactor-validations/blob/master/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("juraji")
+                        name.set("Juraji")
+                        email.set("github@juraji.nl")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Juraji/reactor-validations.git")
+                    developerConnection.set("scm:git:ssh://github.com/Juraji/reactor-validations.git")
+                }
+            }
+        }
+    }
+}
+
 tasks.withType<Test> {
     useJUnitPlatform()
 }
